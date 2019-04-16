71ef96edbf0e2f9884f0630c28bca7c7481264c4
exiaohao <exiaohao@gmail.com>
2019-04-16 14:13:47 +0800

    Ignore InitializeKubeClient

--- pkg/common/k8s.go
+++ pkg/common/k8s.go
@@ -1,16 +1,12 @@
 package common
 
-import (
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-)
-
 
 // InitializeKubeClient by kubeconfig
-func InitializeKubeClient(kubeconfigPath string) (*kubernetes.Clientset, error) {
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
-	if err != nil {
-		return nil, err
-	}
-	return kubernetes.NewForConfig(config)
-}
+// kubernetes client version is old, need upgrade
+//func InitializeKubeClient(kubeconfigPath string) (*kubernetes.Clientset, error) {
+//	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+//	if err != nil {
+//		return nil, err
+//	}
+//	return kubernetes.NewForConfig(config)
+//}
